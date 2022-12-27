f716b9372f97bfc2ccb780dc32a036efa63fe2ff
alkarathod07 <alkarathod.rathod1@gmail.com>
2022-12-27 11:36:10 +0530

    Changes by alka

---
+++ src/main/java/com/BikkadIT/Controller/BookController.java
@@ -0,0 +1,27 @@
+package com.BikkadIT.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.BikkadIT.Entity.Book;
+
+
+@Controller
+@RequestMapping("/api")
+public class BookController {
+	
+	@GetMapping(value="/book")
+	public String getBook(Model model) {
+		Book  b=new Book();
+		b.setBookId(101);
+		b.setName("java");
+		b.setBookPrice(150);
+		
+		model.addAttribute("book", b);
+		return "book";
+		
+	}
+
+}
